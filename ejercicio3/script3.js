d2880ad5e6637a2d5a797bf7e54984c4166bcace
agustinluz <agustinluzmartinll@gmail.com>
2024-10-14 11:48:06 +0200

    seguir

---
+++ ejercicio3/script3.js
@@ -0,0 +1,6 @@
+function ej3(){
+    var titulo=document.title
+    console.log("El titulo es: "+titulo.toUpperCase());
+    document.getElementById('nombre').value="Agustín";
+    document.getElementById('apellido').value="Luz";
+}
