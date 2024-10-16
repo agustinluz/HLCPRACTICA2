28da7c7e964f5f3b59db95533deddbb5245fdaf4
agustinluz <agustinluzmartinll@gmail.com>
2024-10-16 11:32:38 +0200

    ejercicio3

--- ejercicio3/script3.js
+++ ejercicio3/script3.js
@@ -1,6 +1,19 @@
-function ej3(){
-    var titulo=document.title
-    console.log("El titulo es: "+titulo.toUpperCase());
-    document.getElementById('nombre').value="Agustín";
-    document.getElementById('apellido').value="Luz";
-}
+window.onload = function() {
+    var titulo = document.getElementsByTagName("title")[0].textContent;
+    console.log(titulo.toUpperCase());
+  
+    document.getElementById('nombre').value = "Agustin"; 
+    document.getElementById('apellido').value = "Luz"; 
+  
+    var nombre = document.getElementById('nombre').value;
+    var apellido = document.getElementById('apellido').value;
+    var saludo = document.getElementById('saludo');
+    saludo.textContent = `Hola ${nombre} ${apellido}.`;
+  
+    var parrafo = document.createElement('p');
+    parrafo.textContent = "¿Qué tal estás?";
+    saludo.parentNode.insertBefore(parrafo, saludo.nextSibling);
+  
+    var apellido = document.querySelector('label[for="apellido"]');
+    apellido.textContent = "Apellidos:";
+};
